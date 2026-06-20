4ef50780c77c56e4ee29c8f7ed0c5ff300eb5384
RAYINFN <chwch1860@gmail.com>
2026-06-20 00:49:38 -0700

    Update StaffProfile.java

--- src/main/java/com/cmpt276/asn2/StaffMemberProfile/StaffProfile.java
+++ src/main/java/com/cmpt276/asn2/StaffMemberProfile/StaffProfile.java
@@ -1,6 +1,7 @@
 package com.cmpt276.asn2.StaffMemberProfile;
 
 import com.cmpt276.asn2.Role.Role;
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -11,13 +12,23 @@ public class StaffProfile {
 
     @NotNull(message = "Role is required")
     @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "varchar(255)")
+    
     private Role role;
-
     private String profileTitle;
 
-    public Role getRole() { return role; }
-    public void setRole(Role role) { this.role = role; }
-
-    public String getProfileTitle() { return profileTitle; }
-    public void setProfileTitle(String profileTitle) { this.profileTitle = profileTitle; }
+    public Role getRole() {
+        return role;
+    }
+    
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    public String getProfileTitle() {
+        return profileTitle;
+    }
+    
+    public void setProfileTitle(String profileTitle) {
+        this.profileTitle = profileTitle;
+    }
 }
